d71156ff984b722d97109dde0658a68da174c9e6
gagan517 <gagankumarsharma812@gmail.com>
2025-05-17 18:02:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; // Player O starts
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+const resetGame = () => {
+    turnO = true; 
+    boxes.forEach((box) => {
+        box.innerText = "";
+        box.disabled = false;
+    });
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (box.innerText !== "") return; // Prevent overwriting an already filled box
+
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            box.innerText = "X"; // Changed to "X" from "G" for clarity
+            turnO = true;
+        }
+
+        box.disabled = true;
+        checkWinner();
+    });
+});
+
+const disableBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = true;
+    });
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val); // If all three positions match, there's a winner
+                return;
+            }
+        }
+    }
+
+    // Check for a draw (if all boxes are filled and no winner)
+    let allFilled = true;
+    boxes.forEach((box) => {
+        if (box.innerText === "") {
+            allFilled = false;
+        }
+    });
+
+    if (allFilled) {
+        msg.innerText = "It's a Draw!";
+        msgContainer.classList.remove("hide");
+        disableBoxes();
+    }
+};
+
+resetBtn.addEventListener("click", resetGame);
+newGameBtn.addEventListener("click", resetGame);
